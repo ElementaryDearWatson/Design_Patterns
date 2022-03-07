ea897e57b86b240e5a7a72a9730d59e34cac6882
Nick Nalli <nalli.nikhil@gmail.com>
2022-03-06 20:56:49 -0600

    Added the singleton pattern with lazy instantiation

---
+++ SingletonPatterns/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingletonPatterns
+{
+    public class Program
+    {
+        static readonly Servers host1List = Servers.GetTableServers();
+        static readonly Servers host2List = Servers.GetTableServers();
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server for Host 1 is: " + host1List.GetNextServer());
+        }
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server for Host 2 is: " + host2List.GetNextServer());
+        }
+    }
+}
---
+++ SingletonPatterns/Servers.cs
@@ -0,0 +1,34 @@
+﻿namespace SingletonPatterns
+{
+    internal class Servers
+    {
+        private static readonly Servers _instance = new Servers();
+        private Dictionary<int, string> tableServers = new Dictionary<int, string>();
+        private int nextServer = 0;
+
+        private Servers()
+        {
+            tableServers.Add(1, "Bob");
+            tableServers.Add(2, "Tim");
+            tableServers.Add(3, "Manny");
+            tableServers.Add(4, "Mary");
+            tableServers.Add(5, "Katie");
+        }
+
+        public static Servers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string GetNextServer()
+        {
+            string output = tableServers.ElementAt(nextServer).Value;
+            nextServer+=1;
+            if (nextServer >= tableServers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+        }
+    }
+}
